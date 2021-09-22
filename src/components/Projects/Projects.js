ef9c28fa13fa7cd5c3245012852f6a8960c77cf8
Tanay <tbhatia_be20@thapar.edu>
2021-09-22 17:26:36 +0530

    text changed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="WORKSHOPS"
-              description="We at LEAD believe in creating an impact thorugh knowledge.To keep up with this,LEAD conducted weekly workshops for its members, Where expersts from various fields shared their experience in fields like graphic designing,content writing etc"
+              description="We at LEAD believe in creating an impact through knowledge. To keep up with this, LEAD conducted weekly workshops for its members, where experts from various fields shared their experience in graphic designing, content writing etc"
               
             />
           </Col>
@@ -37,7 +37,7 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title="FLUTTER WORKSHOP"
-              description="Another mind-boggling workshop was organized by LEAD on 14th and 15th January 2020,with a motive to enhance technical skills and promote app development among young minds.Participants were given insights to Flutter enviorment setups."
+              description="Another mind-boggling workshop was organized by LEAD on 14th and 15th January 2020, with a motive to enhance technical skills and promote app development among young minds. Participants were given insights to Flutter environment setups."
               
             />
           </Col>
@@ -47,7 +47,7 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="ALUMNI TALKS"
-              description="Web Set Go was concluded with 2 days of information interaction with some of the most prestigious alumni of Thapar and some external guests.They shared their life experiences in various Technical(web dev, app dev, programming) and Non-Technical (leadership, public speaking, presentation) skills"
+              description="Web Set Go was concluded with two days of informal interaction with some of the most prestigious alumni of Thapar and some external guests. They shared their life experiences in various Technical (web dev, app dev, programming) and non-Technical (leadership, public speaking, presentation) skills"
               
             />
           </Col>
@@ -57,7 +57,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="SHOONYA"
-              description="The most exciting and captivating event, 'SHOONYA 2.0' was hosted by LEAD.It was a week-long event which involved a series of events that tested one's technical,non-technical and reasoning skills. It was a flagship event, and has always been looked fowrad by the audience "
+              description="The most exciting and captivating event, 'SHOONYA 2.0' was hosted by LEAD. It was a week-long event that involved a series of events that tested one's technical, non-technical and reasoning skills. It was a flagship event and has always been looked forward to by the audience. "
              
             />
           </Col>
@@ -77,9 +77,7 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="THE WEBBIES"
-              description="The most awaited journey into the wide realms of webdevelopment started with a three-day workshop on web
-              development from scratch. The event started with an enlightening HTML+CSS and JavaScript workshop
-              followed by it's implementation in a mini-project."
+              description="The most awaited journey into the diverse realms of web development started with a three-day workshop on web development from scratch. The event started with an enlightening HTML+CSS and JavaScript workshop, followed by its implementation in a mini-project."
               
             />
           </Col>
